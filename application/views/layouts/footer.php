64a4841a0d35ee177e5d8f4bafe76cf102bd3ae2
ahmad208 <72955012+ahmad208@users.noreply.github.com>
2021-05-07 23:40:21 +0700

    initial commit

---
+++ application/views/layouts/footer.php
@@ -0,0 +1,14 @@
+                </div>
+            </div>
+        </div>
+        <footer>
+            <p style= "text-align: center;">Ahmad Luthfi Taqiyuddin</p>
+            <p style= "text-align: center;">TI-03</p>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="<?= base_url('public/js/scripts.js')?> "></script>
+    </body>
+</html>
